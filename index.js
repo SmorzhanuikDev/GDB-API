028b1d5f8b97b11b635eb5f9fde094dc634bcf6d
Smorzhaniuk <smorzhaniuk.cinis@gmail.com>
2024-12-22 17:15:25 +0200

    some commit

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = 3000
 app.get('/', (req, res) => {
     res.json({test: 'this shit works, and after nodemon adding works to'})
 })
-app.get('/', (req, res) => {
-    res.json({test: 'this shit works, and after nodemon adding works to'})
-})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
