bda770937488ad07db764c7065ed3f25fdb17801
Smorzhaniuk <smorzhaniuk.cinis@gmail.com>
2024-12-22 18:35:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,37 @@ app.use(express.json())
 
 const port = 3000
 
+const users = [
+    {
+        login: 'testUser',
+        name: 'Evan',
+        password: '123456',
+        token: '123456',
+        gameLists: [
+            {
+                name: 'my games',
+                gamesId: [432, 3234]
+            }
+        ]
+    },
+]
+
+
 app.get('/', (req, res) => {
+    res.json(users)
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    users.push(req.body)
+    res.json({test: 'this shit works, and after nodemon adding works to'})
+})
+
+app.put('/', (req, res) => {
+    res.json({test: 'this shit works, and after nodemon adding works to'})
+})
+
+app.delete('/', (req, res) => {
     res.json({test: 'this shit works, and after nodemon adding works to'})
 })
 
